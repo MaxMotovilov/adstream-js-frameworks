bad774776367610a36a43c1776b1e4ee4519da16
Max <max@motovilov.com>
2012-12-24 13:13:46 -0600

    Added top-level modules for adstream.data and adstream.navigator

---
+++ libs/adstream/data.js
@@ -0,0 +1,7 @@
+// Copyright (C) 2012 Adstream Holdings
+// All rights reserved.
+// Redistribution and use are permitted under the modified BSD license
+// available at https://github.com/MaxMotovilov/adstream-js-frameworks/wiki/License
+
+dojo.provide( 'adstream.data' );
+dojo.require( 'adstream.data.Service' );
---
+++ libs/adstream/navigator.js
@@ -0,0 +1,7 @@
+// Copyright (C) 2012 Adstream Holdings
+// All rights reserved.
+// Redistribution and use are permitted under the modified BSD license
+// available at https://github.com/MaxMotovilov/adstream-js-frameworks/wiki/License
+
+dojo.provide( 'adstream.navigator' );
+dojo.require( 'adstream.navigator.core' );
