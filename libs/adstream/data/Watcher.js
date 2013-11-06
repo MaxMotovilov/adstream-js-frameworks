68207a044f7dcbd7de21347d7d7af7a92f3a1c4b
Max <max@motovilov.com>
2013-11-06 12:05:02 -0600

    Revert "Moved the hook from destroy() to uninitialize() to conform to spec." Reason: uninitialize() is deprecated; also makes it work w/o WidgetBase
    This reverts commit 8cb041cfc9ca6a626cfb038b55b4c68fa0e76eff.

--- libs/adstream/data/Watcher.js
+++ libs/adstream/data/Watcher.js
@@ -34,7 +34,7 @@ dojo.declare( 'adstream.data.Watcher', null, {
 		dojo.forEach( del, function(ru){ delete this._dataWatches[ ru ]; }, this );
 	},
 
-	uninitialize: function() {
+	destroy: function() {
 		for( var rel_url in this._dataWatches )
 			this._dataWatches[rel_url].ignore( rel_url );
 		return this.inherited( arguments );
